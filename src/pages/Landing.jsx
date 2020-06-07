6ad0a9363ad8fcccfc037d732d4dcd88284a8abb
Gareth Williams <gareth@Gareths-MacBook-Pro.local>
2020-06-07 16:51:56 +0100

    [#0] Add landing page and initial colour scheme.
    [@gazeddyw]

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+const Landing = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>['MILLAGE_WRAITHS_CLUB']</code>
+        </p>
+        <p>
+          <code>></code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
